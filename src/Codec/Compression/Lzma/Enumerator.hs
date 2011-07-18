916086ea95e76c5cb2b37abfde659ed54f99df80
Nathan Howell <nhowell@alphaheavy.com>
2011-07-17 18:00:23 -0700

    Initial checkin

---
+++ src/Codec/Compression/Lzma/Enumerator.hs
@@ -0,0 +1,117 @@
+module Codec.Compression.Lzma.Enumerator
+  ( compress
+  ) where
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Unsafe as B
+import Control.Monad
+import Control.Monad.Trans
+import qualified Data.Enumerator as E
+import qualified Data.Enumerator.List as EL
+import Debug.Trace (trace)
+import Foreign
+import Foreign.C.String
+import Foreign.C.Types
+import Foreign.Storable
+import Bindings.Lzma
+
+prettyRet :: C'lzma_ret -> String
+prettyRet r
+  | r == c'LZMA_OK                 = "Operation completed successfully"
+  | r == c'LZMA_STREAM_END         = "End of stream was reached"
+  | r == c'LZMA_NO_CHECK           = "Input stream has no integrity check"
+  | r == c'LZMA_UNSUPPORTED_CHECK  = "Cannot calculate the integrity check"
+  | r == c'LZMA_GET_CHECK          = "Integrity check type is now available"
+  | r == c'LZMA_MEM_ERROR          = "Cannot allocate memory"
+  | r == c'LZMA_MEMLIMIT_ERROR     = "Memory usage limit was reached"
+  | r == c'LZMA_FORMAT_ERROR       = "File format not recognized"
+  | r == c'LZMA_OPTIONS_ERROR      = "Invalid or unsupported options"
+  | r == c'LZMA_DATA_ERROR         = "Data is corrupt"
+  | r == c'LZMA_BUF_ERROR          = "No progress is possible"
+  | r == c'LZMA_PROG_ERROR         = "Programming error"
+  | otherwise                      = "Unknown LZMA error: "++show r
+
+bufferSize :: Num a => a
+bufferSize = 4096
+
+compress :: MonadIO m
+         => E.Enumeratee B.ByteString B.ByteString m b
+compress step = trace "compress" $ do
+  streamPtr <- E.tryIO $ do
+    buffer <- mallocBytes bufferSize
+    streamPtr <- new C'lzma_stream
+          { c'lzma_stream'next_in   = nullPtr
+          , c'lzma_stream'avail_in  = 0
+          , c'lzma_stream'total_in  = 0
+          , c'lzma_stream'next_out  = buffer
+          , c'lzma_stream'avail_out = bufferSize 
+          , c'lzma_stream'total_out = 0 }
+    ret <- c'lzma_easy_encoder streamPtr c'LZMA_PRESET_DEFAULT c'LZMA_CHECK_CRC64
+    if ret == c'LZMA_OK
+      then return streamPtr
+      else fail $ "c'lzma_easy_encoder failed: " ++ prettyRet ret
+
+  compress' streamPtr step
+
+buildChunks :: Ptr C'lzma_stream
+            -> C'lzma_action
+            -> IO (E.Stream B.ByteString)
+buildChunks streamPtr action = liftM E.Chunks go where
+  go = do
+    ret <- c'lzma_code streamPtr action 
+    if ret /= c'LZMA_OK && ret /= c'LZMA_STREAM_END
+      then fail $ "c'lzma_code failed: " ++ prettyRet ret
+      else do
+        availIn <- peekAvailIn streamPtr
+        availOut <- peekAvailOut streamPtr
+        case () of
+          _ | availIn > 0 && availOut > 0 -> go
+            | (availIn > 0 && availOut == 0) ||
+              (action == c'LZMA_FINISH && availOut < bufferSize) -> do
+                chunk <- getOutChunk streamPtr
+                tail <- go
+                return $ chunk:tail
+            | otherwise -> return []
+
+compress' :: MonadIO m
+          => Ptr C'lzma_stream
+          -> E.Enumeratee B.ByteString B.ByteString m b
+compress' streamPtr (E.Continue k) = do
+  chunk <- EL.head
+  chunks <- E.tryIO $ case chunk of
+    Just chunk' ->
+      B.unsafeUseAsCStringLen chunk' $ \ (ptr, len) -> do
+        pokeNextIn streamPtr ptr
+        pokeAvailIn streamPtr $ fromIntegral len
+        buildChunks streamPtr c'LZMA_RUN
+    Nothing -> do
+      availIn <- peekAvailIn streamPtr
+      availOut <- peekAvailOut streamPtr
+      if availIn > 0 || availOut < bufferSize
+        then buildChunks streamPtr c'LZMA_FINISH
+        else return E.EOF
+  step <- lift $ E.runIteratee (k chunks)
+  compress' streamPtr step
+
+compress' streamPtr step = do
+  E.tryIO $ do
+    free =<< peekNextOut streamPtr
+    c'lzma_end streamPtr
+  return step
+
+getOutChunk :: Ptr C'lzma_stream
+            -> IO B.ByteString
+getOutChunk streamPtr = do
+  availOut <- peekAvailOut streamPtr
+  if availOut < bufferSize
+    then do
+      nextOut <- peekNextOut streamPtr
+      let avail = bufferSize - fromIntegral availOut
+          baseBuffer = nextOut `plusPtr` (-avail)
+      bs <- B.packCStringLen (baseBuffer, avail)
+      pokeAvailOut streamPtr bufferSize
+      -- B.pack* copies the buffer, so reuse it
+      pokeNextOut streamPtr baseBuffer
+      return bs
+    else return B.empty
+
